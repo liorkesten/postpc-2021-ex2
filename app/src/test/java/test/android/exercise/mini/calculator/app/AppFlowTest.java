f6ada7b1d9598fa4834d5e565cdf1e4865e9bcee
Lior Kesten <t-liorkesten@microsoft.com>
2021-03-30 00:06:52 +0300

    FlowTests Done, bug with backspace after equal and when operator before digit

--- app/src/test/java/test/android/exercise/mini/calculator/app/AppFlowTest.java
+++ app/src/test/java/test/android/exercise/mini/calculator/app/AppFlowTest.java
@@ -14,6 +14,7 @@ import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -89,5 +90,115 @@ public class AppFlowTest {
     assertEquals("11", textViewOutput.getText().toString());
   }
 
+  @Test
   // TODO: add at last 10 more flow tests
+  public void flowTest3(){
+    // run clicks on "-7+5++3"
+    for (View button: Arrays.asList(
+            buttonMinus,button7, buttonPlus, button5, buttonPlus, buttonPlus, button3
+    )) {
+      button.performClick();
+    }
+    assertEquals("0-7+5+3", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest4(){
+    // run clicks on "---1+2"
+    for (View button: Arrays.asList(
+            buttonMinus,buttonMinus,buttonMinus,button1,buttonPlus,button2
+    )) {
+      button.performClick();
+    }
+    assertEquals("0-1+2", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest5(){
+    // run clicks on "3+2=<BackSpace><BackSpace>+2="
+    for (View button: Arrays.asList(
+            button3,buttonPlus,buttonEquals,buttonBackspace,buttonBackspace,buttonPlus,button2,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("7", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest6(){
+    // run clicks on "002+3="
+    for (View button: Arrays.asList(
+            button0,button0,button2,buttonPlus,button3,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("5", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest7(){
+    // run clicks on "1-+2="
+    for (View button: Arrays.asList(
+            button1,buttonMinus,buttonPlus,button2,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("-1", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest8(){
+    // run clicks on "1+1=<clear>="
+    for (View button: Arrays.asList(
+            button1,buttonPlus,button1,buttonEquals,buttonClear,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("0", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest9(){
+    // run clicks on "+="
+    for (View button: Arrays.asList(
+            buttonPlus,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("0", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest10(){
+    // run clicks on "2+="
+    for (View button: Arrays.asList(
+            button2,buttonPlus,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("2", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest11(){
+    // run clicks on "<BackSpace>"
+    for (View button: Collections.singletonList(
+            buttonBackspace
+    )) {
+      button.performClick();
+    }
+    assertEquals("0", textViewOutput.getText().toString());
+  }
+
+  @Test
+  public void flowTest12(){
+    // run clicks on "1+1+1-<backspace>0+1="
+    for (View button: Arrays.asList(
+            button1,buttonPlus,button1,buttonPlus,button1,buttonMinus,buttonBackspace,button0
+            ,buttonPlus,button1,buttonEquals
+    )) {
+      button.performClick();
+    }
+    assertEquals("13", textViewOutput.getText().toString());
+  }
 }
